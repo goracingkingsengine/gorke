f7c1d79e6e469d2a8a9d3d48500b416dfa8875da
goracingkingsengine <goracingkingsengine@gmail.com>
2016-01-18 12:53:08 +0100

    Update

--- gorke.go
+++ gorke.go
@@ -5,8 +5,10 @@ import(
 	"os"
 	"bufio"
 	"strings"
+	"strconv"
 	"text/scanner"
 	"github.com/goracingkingsengine/gorke/piece"
+	"github.com/goracingkingsengine/gorke/square"
 )
 
 func main() {
@@ -36,6 +38,8 @@ func main() {
 			if command=="list" {
 				fmt.Print("list - list commands\n")
 				fmt.Print("ftop - fenchar to piece\n")
+				fmt.Print("atos - algeb to square\n")
+				fmt.Print("stoa - square to algeb\n")
 				fmt.Print("x - exit\n")
 			}
 
@@ -51,6 +55,28 @@ func main() {
 				var p=piece.FromFenChar(fenchar)
 
 				fmt.Printf("piece code %d type %d color %d",p,piece.TypeOf(p),piece.ColorOf(p))
+			}
+
+			if command=="atos" {
+				tok=s.Scan()
+
+				if tok!=scanner.EOF {
+					var s=square.FromAlgeb(s.TokenText())
+
+					fmt.Printf("square %d",s)
+				}
+			}
+
+			if command=="stoa" {
+				tok=s.Scan()
+
+				if tok!=scanner.EOF {
+					sq,err:=strconv.Atoi(s.TokenText())
+
+					if err==nil {
+						fmt.Printf("algeb %s",square.ToAlgeb(square.TSquare(sq)))
+					}
+				}
 			}
 		}
 
---
+++ square/square.go
@@ -0,0 +1,79 @@
+package square
+
+import "fmt"
+
+const BOARD_WIDTH         = 8
+const BOARD_WIDTHL        = BOARD_WIDTH-1
+
+const BOARD_HEIGHT        = BOARD_WIDTH
+const BOARD_HEIGHTL       = BOARD_HEIGHT-1
+
+const BOARD_SIZE          = BOARD_WIDTH * BOARD_HEIGHT
+const BOARD_SIZEL         = BOARD_SIZE-1
+
+const NO_SQUARE           = BOARD_SIZE
+
+const BOARD_SHIFT         = 3
+
+const FILE_MASK           = BOARD_WIDTHL
+const RANK_MASK           = FILE_MASK << BOARD_SHIFT
+
+type TSquare byte
+type TFile byte
+type TRank byte
+
+func FileOf(sq TSquare) TFile {
+	return TFile(byte(sq)&FILE_MASK)
+}
+
+func RankOf(sq TSquare) TRank {
+	return TRank((byte(sq)&RANK_MASK)>>BOARD_SHIFT)
+}
+
+func AlgebFileToFile(af byte) (TFile, bool) {
+	if (af<'a') || (af>('a'+BOARD_WIDTHL)) {
+		return 0, true
+	}
+	return TFile(af-'a'), false
+}
+
+func FileToAlgebFile(f TFile) byte {
+	return byte('a'+byte(f))
+}
+
+func AlgebRankToRank(ar byte) (TRank, bool) {
+	if (ar<'1') || (ar>('1'+BOARD_HEIGHTL)) {
+		return 0, true
+	}
+	return TRank(BOARD_HEIGHTL-(byte(ar)-'1')), false
+}
+
+func RankToAlgebRank(r TRank) byte {
+	return byte('1'+(BOARD_HEIGHTL-byte(r)))
+}
+
+func FromFileRank(f TFile, r TRank) TSquare {
+	return TSquare(byte(f)|(byte(r)<<BOARD_SHIFT))
+}
+
+func FromAlgeb(algeb string) TSquare {
+	if len(algeb)<2 {
+		return NO_SQUARE
+	}
+	f,err:=AlgebFileToFile(algeb[0])
+	if(err) {
+		return NO_SQUARE
+	}
+	r,err:=AlgebRankToRank(algeb[1])
+	if(err) {
+		return NO_SQUARE
+	}
+	return FromFileRank(f,r)
+}
+
+func ToAlgeb(sq TSquare) string {
+	if (sq<0) || (sq>BOARD_SIZEL) {
+		return "-"
+	}
+	return fmt.Sprintf("%c%c",FileToAlgebFile(FileOf(sq)),RankToAlgebRank(RankOf(sq)))
+}
